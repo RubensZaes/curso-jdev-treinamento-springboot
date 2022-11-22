a458e2448f63a1eb0e2f2d455346ce1c3177eda9
Rubens Stanley Almeida Brandão <rubensalmeida@sefaz.pi.gov.br>
2022-11-22 11:15:08 -0300

    First Commit

---
+++ src/main/java/br/com/springboot/cursojdevtreinamentospringboot/CursoJDevTreinamentoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.springboot.cursojdevtreinamentospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoJDevTreinamentoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoJDevTreinamentoSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/springboot/cursojdevtreinamentospringboot/CursoJDevTreinamentoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.springboot.cursojdevtreinamentospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoJDevTreinamentoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
